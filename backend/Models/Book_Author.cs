4d913925462716638a6a523e08442eb93f1ca9ec
Ani <anitarominawuensch@gmail.com>
2018-08-24 15:27:34 +0200

    add models Book_Genre/Author

--- backend/Models/Book_Author.cs
+++ backend/Models/Book_Author.cs
@@ -1,6 +1,5 @@
-
 namespace SakurAni_Lib.Models {
-    public class BookAuthor {
+    public class Book_Author {
         public int Id { get; set; }
         public string Isbn {get; set;}
         public string AuthorId {get; set;}
---
+++ backend/Models/Book_Genre.cs
@@ -0,0 +1,9 @@
+
+namespace SakurAni_Lib.Models {
+    using System.Collections.Generic;
+    public class Book_Genre {
+        public string Id {get; set;}
+        public string Isbn {get; set;}
+        public string GenreId {get; set;}
+    }
+}
